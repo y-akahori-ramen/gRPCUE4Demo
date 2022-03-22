2491bd6a46b03a5cff3c05b8af661a76fbd5ebdf
y-akahori-ramen <y.akahori.ramen@gmail.com>
2022-03-23 01:14:13 +0900

    UEプロジェクト作成

---
+++ gRPCDemoProject/Source/gRPCDemoProject/gRPCDemoProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "gRPCDemoProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GRPCDEMOPROJECT_API AgRPCDemoProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
