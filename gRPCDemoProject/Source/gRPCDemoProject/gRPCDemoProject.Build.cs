7a82f8ca1596446cefa8b10d63919924785e384b
y-akahori-ramen <y.akahori.ramen@gmail.com>
2022-04-02 16:54:06 +0900

    UE側でgRPCプラグインを使用できるように対応

--- gRPCDemoProject/Source/gRPCDemoProject/gRPCDemoProject.Build.cs
+++ gRPCDemoProject/Source/gRPCDemoProject/gRPCDemoProject.Build.cs
@@ -19,5 +19,7 @@ public class gRPCDemoProject : ModuleRules
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
 		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+
+		PublicDependencyModuleNames.Add("gRPCForUE4");
 	}
 }
