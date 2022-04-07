b5b4d839d483cd9a12b9c602f57a829a53999699
y-akahori-ramen <y.akahori.ramen@gmail.com>
2022-04-08 01:46:50 +0900

    不要な環境変数の削除

--- setup.bat
+++ setup.bat
@@ -6,7 +6,6 @@ git submodule update --init
 
 setlocal
 echo "Setup gRPC Plugin"
-set GRPC_ROOT=%~dp0\gRPCDemoProject\Plugins\gRPCForUE4\Setup\grpcsrc\grpc
 set GRPC_BRANCH=v1.41.1
 call .\gRPCDemoProject\Plugins\gRPCForUE4\Setup\setup.bat
 endlocal
